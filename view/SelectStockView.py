a8cbcfc1d027890eb5b1bc8b70705b1269348805
chungzon <chungzon@gmail.com>
2025-08-07 01:39:56 +0800

    export json 扣抵值

--- view/SelectStockView.py
+++ view/SelectStockView.py
@@ -1503,7 +1503,10 @@ class SelectStockView(tk.Frame):
             ('周', '120'), ('月', '5'), ('周', '10'), ('日', '20'),
             'AL', ('周', '5'), 'NL', ('15K', '20'), 'CDP', ('15K', '10'),
             ('15K', '5'), ('15K', 'strong'), ('15K', 'weak'), ('日', '10'), ('日', '5'), 'NH', 'AH',
-            ('月', '10'), ('月', '20'), ('月', '60'), ('月', '120')
+            ('月', '10'), ('月', '20'), ('月', '60'), ('月', '120'),
+            ('日', '5MA_DIFF'), ('日', '10MA_DIFF'), ('日', '20MA_DIFF'), ('日', '60MA_DIFF'), ('日', '120MA_DIFF'),
+            ('周', '5MA_DIFF'), ('周', '10MA_DIFF'), ('周', '20MA_DIFF'), ('周', '60MA_DIFF'), ('周', '120MA_DIFF'),
+            ('月', '5MA_DIFF'), ('月', '10MA_DIFF'), ('月', '20MA_DIFF'), ('月', '60MA_DIFF'), ('月', '120MA_DIFF')
         ]
         
         # 添加均線和指標數據
@@ -1513,8 +1516,11 @@ class SelectStockView(tk.Frame):
                 key = f"{prefix}({period})_DC"
                 if prefix in {'日': '日均線', '周': '週均線', '月': '月均線', '15K': '15分鐘均線'}:
                     ma_type = {'日': '日均線', '周': '週均線', '月': '月均線', '15K': '15分鐘均線'}[prefix]
+                    # 處理扣抵值
+                    if period.endswith('MA_DIFF'):
+                        ma_period = period.replace('MA_DIFF', 'MA_DIFF')
                     # 對於15分鐘均線，需要特殊處理strong和weak
-                    if prefix == '15K' and period in ['strong', 'weak']:
+                    elif prefix != '月' and period in ['strong', 'weak']:
                         ma_period = period
                     else:
                         ma_period = f"{period}MA"
